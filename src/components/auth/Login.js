e0e0dde7f9db62169272a55e7ed73888a133bc8a
AJ-Romera <ajromeraalfonso@gmail.com>
2021-03-17 19:52:50 +0100

    CHNG: Construye el Formulario del Componente Login

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,7 +1,47 @@
 import React from 'react';
 
 function Login() {
-    return <h1>Desde Login</h1>;
+    const handleLogin = () => {};
+
+    return (
+        <div className='form-usuario'>
+            <div className='contenedor-form sombra-dark'>
+                <h1>Iniciar Sesión</h1>
+
+                <form>
+                    <div className='campo-form'>
+                        <label htmlFor='email'>Email</label>
+                        <input
+                            type='email'
+                            id='email'
+                            name='email'
+                            placeholder='Tu Email'
+                            onChange={handleLogin}
+                        />
+                    </div>
+
+                    <div className='campo-form'>
+                        <label htmlFor='password'>Password</label>
+                        <input
+                            type='password'
+                            id='password'
+                            name='password'
+                            placeholder='Tu Password'
+                            onChange={handleLogin}
+                        />
+                    </div>
+
+                    <div className='campo-form'>
+                        <input
+                            type='submit'
+                            className='btn btn-primario btn-block'
+                            value='Iniciar Sesión'
+                        />
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default Login;
