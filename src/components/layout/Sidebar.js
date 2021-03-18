120f02c3ccc4ffa41fd8e1d3b25ccd029395e312
AJ-Romera <ajromeraalfonso@gmail.com>
2021-03-18 18:13:24 +0100

    CHNG: Hace Estructura Componente Proyectos && Componente Sidebar del Layout

---
+++ src/components/layout/Sidebar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Sidebar() {
+    return (
+        <aside>
+            <h1>
+                <span>Tareas</span>MERN
+            </h1>
+
+            <div className="proyectos">
+                <h2>Tus Proyectos</h2>
+            </div>
+        </aside>
+    );
+}
+
+export default Sidebar;
