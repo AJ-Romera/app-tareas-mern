8a402fefb6ca12469e35618c1dcd2c3c9b260a49
AJ-Romera <ajromeraalfonso@gmail.com>
2021-03-21 13:29:54 +0100

    CNHG: Agrega la Fn para Mostrar el Formulario

--- src/components/proyectos/NuevoProyecto.js
+++ src/components/proyectos/NuevoProyecto.js
@@ -4,7 +4,7 @@ import proyectoContext from '../../context/proyectos/proyectoContext';
 function NuevoProyecto() {
     // Obtener el state del formulario
     const proyectosContext = useContext(proyectoContext);
-    const { formulario } = proyectosContext;
+    const { formulario, mostrarFormulario } = proyectosContext;
 
     // State para Proyecto
     const [proyecto, setProyecto] = useState({
@@ -33,9 +33,18 @@ function NuevoProyecto() {
         // Reiniciar el form
     };
 
+    // Mostrar el formulario
+    const onClickFormulario = () => {
+        mostrarFormulario();
+    };
+
     return (
         <>
-            <button type='button' className='btn btn-block btn-primario'>
+            <button
+                type='button'
+                className='btn btn-block btn-primario'
+                onClick={onClickFormulario}
+            >
                 Nuevo Proyecto
             </button>
             {formulario ? (
