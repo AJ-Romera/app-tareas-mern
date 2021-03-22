4b408733812310af417c92555a8bddb47fd69ccf
AJ-Romera <ajromeraalfonso@gmail.com>
2021-03-22 16:49:14 +0100

    CHNG: Oculta el Formulario de Tareas si no hay un Proyecto Seleccionado

--- src/components/tareas/FormTarea.js
+++ src/components/tareas/FormTarea.js
@@ -1,18 +1,40 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import proyectoContext from '../../context/proyectos/proyectoContext';
 
 function FormTarea() {
+    // Extraer si un proyecto está activo
+    const proyectosContext = useContext(proyectoContext);
+    const { proyecto } = proyectosContext;
+
+    // Si no hay proyecto seleccionado
+    if (!proyecto) {
+        return null;
+    }
+
+    // Array destructuring para traer el proyecto actual
+    const [proyectoActual] = proyecto;
+
     return (
-        <div className="formulario">
+        <div className='formulario'>
             <form>
-                <div className="contenedor-input">
-                    <input type="text" className='input-text' placeholder='Nombre de la Tarea' name='nombre' />
+                <div className='contenedor-input'>
+                    <input
+                        type='text'
+                        className='input-text'
+                        placeholder='Nombre de la Tarea'
+                        name='nombre'
+                    />
                 </div>
-                <div className="contenedor-input">
-                    <input type="submit" className='btn btn-primario btn-submit btn-block' value='Agregar una Tarea' />
+                <div className='contenedor-input'>
+                    <input
+                        type='submit'
+                        className='btn btn-primario btn-submit btn-block'
+                        value='Agregar una Tarea'
+                    />
                 </div>
             </form>
         </div>
-    )
+    );
 }
 
-export default FormTarea
+export default FormTarea;
