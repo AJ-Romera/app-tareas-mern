5200867aafc8a2820d70e83cff0c802144b48ae5
AJ-Romera <ajromeraalfonso@gmail.com>
2021-04-05 18:03:40 +0200

    CHNG: Crea Context, Reducer y State de Autenticación

---
+++ src/context/autenticacion/authReducer.js
@@ -0,0 +1,19 @@
+/* import {
+    REGISTRO_EXITOSO,
+    REGISTRO_ERROR,
+    OBTENER_USUARIO,
+    LOGIN_EXITOSO,
+    LOGIN_ERROR,
+    CERRAR_SESION,
+} from '../../types/index'; */
+
+/* eslint import/no-anonymous-default-export: [2, {"allowArrowFunction": true}] */
+export default (state, action) => {
+    switch (action.type) {
+        /* case SDGSDFG_DFGSDFG:
+            return {}; */
+
+        default:
+            return state;
+    }
+};
