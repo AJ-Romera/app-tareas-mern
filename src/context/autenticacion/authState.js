57adaa1d15c1bc421aac5ec38ed450f2ba70be07
AJ-Romera <ajromeraalfonso@gmail.com>
2021-04-06 12:36:38 +0200

    CHNG: Envia el Token por Headers && Obtiene el Usuario

--- src/context/autenticacion/authState.js
+++ src/context/autenticacion/authState.js
@@ -3,6 +3,7 @@ import authContext from './authContext';
 import authReducer from './authReducer';
 
 import clienteAxios from '../../config/axios';
+import tokenAuth from '../../config/token';
 
 import {
     REGISTRO_EXITOSO,
@@ -56,13 +57,19 @@ function AuthState(props) {
     const usuarioAutenticado = async () => {
         const token = localStorage.getItem('token');
         if (token) {
-            // TODO: Función para enviar el token por headers
+            // Función para enviar el token por headers
+            tokenAuth(token);
         }
 
         try {
             const respuesta = await clienteAxios.get('/api/auth');
-            console.log(respuesta);
+            /* console.log(respuesta); */
+            dispatch({
+                type: OBTENER_USUARIO,
+                payload: respuesta.data.usuario,
+            });
         } catch (error) {
+            console.log(error.response);
             dispatch({
                 type: LOGIN_ERROR,
             });
