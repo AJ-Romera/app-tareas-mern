65258a54b1ae7d27c03e986adc3249686b85f6e0
AJ-Romera <ajromeraalfonso@gmail.com>
2021-04-07 17:18:36 +0200

    CHNG: Agrega Proyectos desde la REST API

--- src/context/proyectos/proyectoState.js
+++ src/context/proyectos/proyectoState.js
@@ -1,7 +1,5 @@
 import { React, useReducer } from 'react';
 
-import { v4 as uuidv4 } from 'uuid';
-
 import proyectoContext from './proyectoContext';
 import proyectoReducer from './proyectoReducer';
 import {
@@ -12,6 +10,8 @@ import {
     PROYECTO_ACTUAL,
     ELIMINAR_PROYECTO,
 } from '../../types';
+
+import clienteAxios from '../../config/axios';
 
 const ProyectoState = (props) => {
     const proyectos = [
@@ -48,14 +48,21 @@ const ProyectoState = (props) => {
     };
 
     // * Agregar nuevo proyecto
-    const agregarProyecto = (proyecto) => {
-        proyecto.id = uuidv4();
-
-        // Insertar el proyecto en el state
-        dispatch({
-            type: AGREGAR_PROYECTO,
-            payload: proyecto,
-        });
+    const agregarProyecto = async (proyecto) => {
+        try {
+            const resultado = await clienteAxios.post(
+                '/api/proyectos',
+                proyecto
+            );
+            console.log(resultado);
+            // Insertar el proyecto en el state
+            dispatch({
+                type: AGREGAR_PROYECTO,
+                payload: resultado.data,
+            });
+        } catch (error) {
+            console.log(error);
+        }
     };
 
     // * Validación de Formulario
